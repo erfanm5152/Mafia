8adabbefd557ff90565939ed98c983bebff9db7a
erfanm5152 <erfanm5152@gmail.com>
2021-05-31 11:53:29 +0430

    add other Characters

---
+++ src/Psychologist.java
@@ -0,0 +1,10 @@
+public class Psychologist extends Citizen implements Capable{
+    public Psychologist(String name) {
+        super(name);
+    }
+
+    @Override
+    public void move() {
+
+    }
+}
---
+++ src/Sniper.java
@@ -0,0 +1,11 @@
+public class Sniper extends Citizen implements Capable{
+
+    public Sniper(String name) {
+        super(name);
+    }
+
+    @Override
+    public void move() {
+
+    }
+}
